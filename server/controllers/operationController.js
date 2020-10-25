1eb195b6c697f77773990131ab7caa0e5dad5525
José <josemmazzei@gmail.com>
2020-10-25 20:34:29 -0300

    Add functionality to the controller

--- server/controllers/operationController.js
+++ server/controllers/operationController.js
@@ -3,6 +3,10 @@ let sequelize = require('../models/index')['sequelize']
 
 exports.getAllOperations = async (req, res) => {
   try {
+    let operations = await operationModel.findAll({
+      order: sequelize.literal('id DESC'),
+    })
+    return res.json({ operations: operations })
   } catch (error) {
     return res.status(500).send(error.message)
   }
@@ -10,20 +14,58 @@ exports.getAllOperations = async (req, res) => {
 
 exports.getOperationGroup = async (req, res) => {
   try {
+    let limit = req.query.q
+    let operationGroup = await operationModel.findAll({
+      limit: limit,
+      order: sequelize.literal('id DESC'),
+    })
+    res.json({ operationGroup: operationGroup })
   } catch (error) {
     return res.status(500).send(error.message)
   }
 }
 
 exports.getBalance = async (req, res) => {
-  try {
-  } catch (error) {
-    return res.status(500).send(error.message)
+  async function getBalanceByType(type) {
+    try {
+      let amounts = await operationModel.findAll({
+        attributes: ['amount'],
+        where: { type: type },
+      })
+      return amounts.reduce((accum, current) => accum + current.amount, 0)
+    } catch (error) {
+      return null
+    }
+  }
+
+  let incomeBalance = await getBalanceByType('Income')
+  let outcomeBalance = await getBalanceByType('Outcome')
+  if (incomeBalance && outcomeBalance) {
+    let totalBalance = incomeBalance - outcomeBalance
+    res.json({
+      balance: {
+        income: incomeBalance,
+        outcome: outcomeBalance,
+        total: totalBalance,
+      },
+    })
+  } else {
+    return res.status(500)
   }
 }
 
 exports.createOperation = async (req, res) => {
   try {
+    let { concept, amount, date, type } = req.body
+
+    let operation = await operationModel.create({
+      concept: concept,
+      amount: amount,
+      date: date,
+      type: type,
+    })
+
+    res.json({ created: JSON.stringify(operation) })
   } catch (error) {
     return res.status(500).send(error.message)
   }
@@ -31,6 +73,10 @@ exports.createOperation = async (req, res) => {
 
 exports.deleteOperation = async (req, res) => {
   try {
+    let deleted = await sequelize.query(
+      'delete from operations where id = ' + req.params.id,
+    )
+    res.json({ deleted: req.params.id })
   } catch (error) {
     return res.status(500).send(error.message)
   }
@@ -38,6 +84,18 @@ exports.deleteOperation = async (req, res) => {
 
 exports.editOperation = async (req, res) => {
   try {
+    let { concept, amount, date, id } = req.body
+
+    let putResponse = await operationModel.update(
+      { concept: concept, amount: amount, date: date },
+      {
+        where: {
+          id: id,
+        },
+      },
+    )
+
+    res.json({ opedited: putResponse })
   } catch (error) {
     return res.status(500).send(error.message)
   }
