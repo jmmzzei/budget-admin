a28d942c3e93886caf9d30e0f28bcf51adb71be2
José María <josemmazzei@gmail.com>
2020-12-14 11:21:11 -0300

    Add populateOperations to beforeAll in test file

--- server/controllers/operationController.spec.js
+++ server/controllers/operationController.spec.js
@@ -1,12 +1,30 @@
 process.env.NODE_ENV = 'test'
 const app = require('../index')
 const request = require('supertest')
-const { sequelize, operation } = require('../models/index')
+const { sequelize } = require('../models/index')
+const populateOperations = require('../helpers/populateOperations')
 
+beforeAll(async () => {
+  try {
+    await sequelize
+      .sync({ force: true })
+      .then(() => {
+        console.log(
+          'Connection succesfully established with: ' +
+            sequelize.getDatabaseName(),
+        )
+      })
+      .catch(() => {
+        console.log('Cannot connect')
+      })
+    await populateOperations()
+  } catch (e) {
+    throw new Error(e)
+  }
+})
 
 describe('GET /posts ', () => {
   test('It should respond with posts', async () => {
     expect(true).toBe(true)
   })
 })
-
---
+++ server/helpers/populateOperations.js
@@ -0,0 +1,55 @@
+const { operation } = require('../models/index')
+
+async function populateOperations() {
+  try {
+    const response = await operation.bulkCreate([
+      {
+        concept: 'Food',
+        amount: 200,
+        date: new Date(),
+        type: 'Outcome',
+      },
+      {
+        concept: 'Friends',
+        amount: 100,
+        date: new Date(),
+        type: 'Outcome',
+      },
+      {
+        concept: 'Job',
+        amount: 3500,
+        date: new Date(),
+        type: 'Income',
+      },
+      {
+        concept: 'Food',
+        amount: 50,
+        date: new Date(),
+        type: 'Outcome',
+      },
+      {
+        concept: 'Shop',
+        amount: 230,
+        date: new Date(),
+        type: 'Outcome',
+      },
+      {
+        concept: 'Sell',
+        amount: 850,
+        date: new Date(),
+        type: 'Income',
+      },
+    ])
+    if (response.length == 6) {
+      return new Promise((resolve, reject) => {
+        resolve(true)
+      })
+    }
+  } catch (e) {
+    return new Promise((resolve, reject) => {
+      reject(new Error(e))
+    })
+  }
+}
+
+module.exports = populateOperations
